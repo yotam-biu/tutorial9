9be81a22c3c6b55a8262782c8e73f3507a059b05
yotam-biu <nagaryo3@biu.ac.il>
2025-01-02 19:14:31 +0200

    Create model_test.py

---
+++ .tests/model_test.py
@@ -0,0 +1,20 @@
+import joblib
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import yaml
+
+def test_model():
+
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    path = config['path']
+    features = config['features']
+    assert len(features) == 2
+
+    df_test = pd.read_csv(".tests/penguins_test.csv")
+    X = df_test[features]
+    y = df_test["status"] # categorial
+    model = joblib.load(path)
+    prediction = model.predict(X)
+    score = accuracy_score(y, prediction)
+    assert score > 0.75
